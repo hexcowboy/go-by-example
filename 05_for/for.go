e38be030b9a66a428c23fb410e76eaf80493f8ee
hexcowboy <hex@cowboy.dev>
2021-05-16 18:49:12 -0500

    add for project

---
+++ 05_for/for.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
+
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
+
+    for {
+        fmt.Println("loop")
+        break
+    }
+
+    for i := 0; i <= 5; i++ {
+        if i % 2 == 0 {
+            continue
+        }
+        fmt.Println(i)
+    }
+}
