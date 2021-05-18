496dc9500008e0575b17038239510e35ca1feae9
hexcowboy <hex@cowboy.dev>
2021-05-17 19:31:00 -0500

    add switches project

---
+++ 07_switch/switch.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    i := 2
+    fmt.Print("Write ", i, " as ")
+    switch i {
+    case 1:
+        fmt.Println("one")
+    case 2:
+        fmt.Println("two")
+    case 3:
+        fmt.Println("three")
+    }
+
+    switch time.Now().Weekday() {
+    case time.Saturday, time.Sunday:
+        fmt.Println("It's the weekend!")
+    default:
+        fmt.Println("It's a weekday!")
+    }
+
+    t := time.Now()
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("Good morning")
+    default:
+        fmt.Println("Good afternoon/evening")
+    }
+
+    whatAmI := func(i interface{}) {
+        switch t := i.(type) {
+        case bool:
+            fmt.Println(i, "is a bool")
+        case int:
+            fmt.Println(i, "is an int")
+        default:
+            fmt.Printf("Can't identify type of %T\n", t)
+        }
+    }
+
+    whatAmI(10)
+    whatAmI(true)
+    whatAmI("lol")
+}
